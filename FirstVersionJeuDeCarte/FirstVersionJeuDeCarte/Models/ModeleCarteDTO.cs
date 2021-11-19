3b3b8333e42029093f72d28e3de9db677284b78e
Zakaria Hairane <zakaria.hairane@cgifinance.fr>
2021-11-19 10:02:12 +0100

    Première version, gestion d'un seul jeu de carte| pas de notion d'un (jeu de carte) dans la base de données

---
+++ FirstVersionJeuDeCarte/FirstVersionJeuDeCarte/Models/ModeleCarteDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JeuDeCarte.Models
+{
+    public class ModeleCarteDTO
+    {
+        public int id { get; set; }
+        public string CardValue { get; set; }
+        public Boolean DejaTire { get; set; }
+        public String CardCategory { get; set; }
+        public int Remaining { get; set; }
+        public int Discarded { get; set; }
+        public String LastDiscarded { get; set; }
+        public string Image { get; set; }
+
+        
+    }
+    
+}
---
+++ FirstVersionJeuDeCarte/FirstVersionJeuDeCarte/Startup.cs
@@ -0,0 +1,84 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using JeuDeCarte.Models;
+using JeuDeCarte.Services;
+using System.Collections.Generic;
+
+namespace JeuDeCarte
+{
+    public class Startup
+    {
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            
+            services.AddControllers();
+            services.AddDbContext<CarteContext>(opt =>
+                                               opt.UseInMemoryDatabase("CarteList"));
+            services.AddScoped<CarteContext>();
+            services.AddScoped<CarteService>();
+            services.AddScoped<JeuDeCarteService>();
+            //services.AddSwaggerGen(c =>
+            //{
+            //    c.SwaggerDoc("v1", new OpenApiInfo { Title = "JeuDeCarte", Version = "v1" });
+            //});
+        }
+        
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                //app.UseSwagger();
+                //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "JeuDeCarte v1"));
+            }
+
+            
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+
+            var scope = app.ApplicationServices.CreateScope();
+            var context = scope.ServiceProvider.GetService<CarteContext>();
+            AddCartesData(context);
+        }
+        private static void AddCartesData(CarteContext context)
+        {
+            List<string> LettreDeCategory = new List<string>() { "Clubs", "Diamonds", "Hearts", "Spades" };
+            List<string> Values = new List<string>() { "A", "2", "3", "4", "5", "6", "7", "8", "9", "0", "J", "Q", "K" };
+            foreach (string Lettre in LettreDeCategory)
+            {
+                foreach (string value in Values)
+                {
+                    var Carte = new ModeleCarteDTO();
+                    Carte.CardValue = $"{value}{Lettre.Substring(0, 1)}";
+                    Carte.CardCategory = "Spades";
+                    Carte.Image = $"https://deckofcardsapi.com/static/img/{Carte.CardValue}.png";
+                    context.ModeleCartes.Add(Carte);
+                }
+            }
+
+            context.SaveChanges();
+        }
+    }
+}
